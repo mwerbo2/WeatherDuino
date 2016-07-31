d7bd87ae2323bb0fccdbe87326f37b691393bd81
Michael Werbowsky <mwerbo2@gmail.com>
2016-07-31 18:51:54 -0400

    add results from httparty call

--- weatherduino/app/controllers/weather_controller.rb
+++ weatherduino/app/controllers/weather_controller.rb
@@ -1,10 +1,11 @@
+require 'httparty'
+
 class WeatherController < ApplicationController
   def index
     url = "http://api.wunderground.com/api/91ae0e4eb2f59024/conditions/q/NY/New_York.json"
     response = HTTParty.get(url)
     parsed_body = JSON.parse(response.body)
     render json: parsed_body
-
   end
 end
-console.log("this is the response", parsed_body)
+
