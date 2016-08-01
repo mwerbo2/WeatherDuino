a74a6d647c9913698f11bb5d13e7ea95e4bfc01f
Michael Werbowsky <mwerbo2@gmail.com>
2016-08-01 09:12:41 -0400

    added db

--- weatherduino/db/schema.rb
+++ weatherduino/db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160801024905) do
+ActiveRecord::Schema.define(version: 20160801025305) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "saved_locations", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
